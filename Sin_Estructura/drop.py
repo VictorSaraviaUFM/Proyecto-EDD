e737c2a784f3ff93a750fd132dc098b61b31c593
Panito115 <jpmadriz@ufm.edu>
2025-05-06 09:31:15 -0600

    Drop item

---
+++ Sin_Estructura/drop.py
@@ -0,0 +1,73 @@
+from pynput import keyboard
+
+slot_1 = "Espada"
+slot_2 = "Tierra"
+slot_3 = "Hacha"
+slot_4 = "Arco"
+slot_5 = "Metal"
+slot_6 = "Oro"
+slot_7 = "Ballesta"
+slot_8 = "Diamante"
+
+posicion = 1
+
+def mostrar_slot():
+    if posicion == 1:
+        print(slot_1)
+    elif posicion == 2:
+        print(slot_2)
+    elif posicion == 3:
+        print(slot_3)
+    elif posicion == 4:
+        print(slot_4)
+    elif posicion == 5:
+        print(slot_5)
+    elif posicion == 6:
+        print(slot_6)
+    elif posicion == 7:
+        print(slot_7)
+    elif posicion == 8:
+        print(slot_8)
+
+def tirar_slot():
+    global slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8
+
+    if posicion == 1:
+        slot_1 = "{}"
+    elif posicion == 2:
+        slot_2 = "{}"
+    elif posicion == 3:
+        slot_3 = "{}"
+    elif posicion == 4:
+        slot_4 = "{}"
+    elif posicion == 5:
+        slot_5 = "{}"
+    elif posicion == 6:
+        slot_6 = "{}"
+    elif posicion == 7:
+        slot_7 = "{}"
+    elif posicion == 8:
+        slot_8 = "{}"
+    print(posicion, "{}")
+
+def on_press(key):
+    global posicion
+
+    try:
+        if key.char == 'q':
+            tirar_slot()
+    except AttributeError:
+        if key == keyboard.Key.right:
+            posicion = (posicion % 8) + 1  # Si está en 8, vuelve a 1
+            mostrar_slot()
+        elif key == keyboard.Key.left:
+            posicion = (posicion - 2) % 8 + 1  # Si está en 1, vuelve a 8
+            mostrar_slot()
+        elif key == keyboard.Key.esc:
+            print("Saliendo del programa.")
+            return False
+
+mostrar_slot()
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
